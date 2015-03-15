d9a4ee80698bbddf3e483fd0271f903bcf03cce8
Sumeet Das <sumeetdas1992@gmail.com>
2015-03-16 03:11:32 +0530

    initial commit

--- server/routes.js
+++ server/routes.js
@@ -1,76 +1,77 @@
 /**
  * ROUTES
  */
-var user = require('./security/user')
+var user 	= require('./security/user'),
+	router  = require('express').Router()
 
-app.get('/views/:filename', user.isAuthorized, function (request, response) {
+router.get('/views/:filename', user.isAuthorized, function (request, response) {
 	response.render(request.param('filename') + '.jade');
 })
 
-app.get('/views/:path/:filename', user.isAuthorized, function (request, response) {
+router.get('/views/:path/:filename', user.isAuthorized, function (request, response) {
 	response.render(request.param('path') + '/' + request.param('filename') + '.jade');
 })
 
-app.get('/', function(request, response) {
+router.get('/', function(request, response) {
 	response.render(__dirname + '/public/index')
 })
 
-app.get('/auth/local', passport.authenticate('local'), function local (request, response) {
+router.get('/auth/local', passport.authenticate('local'), function local (request, response) {
 	response.redirect('/account')	
 })
 
-app.get('/auth/facebook', passport.authenticate('facebook'))
+router.get('/auth/facebook', passport.authenticate('facebook'))
 
-app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/' }),
+router.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/' }),
 function (request, response) {
  	response.redirect('/account')
 })
 
-app.get('/auth/twitter', passport.authenticate('twitter'))
+router.get('/auth/twitter', passport.authenticate('twitter'))
 
-app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/' }),
+router.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/' }),
 function (request, response) {
  	response.redirect('/account')
 })
 
-app.get('/auth/google', passport.authenticate('google'))
+router.get('/auth/google', passport.authenticate('google'))
 
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }),
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }),
 function (request, response) {
  	response.redirect('/account')
 })
 
-app.get('/auth/github', passport.authenticate('github'))
+router.get('/auth/github', passport.authenticate('github'))
 
-app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/' }),
+router.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/' }),
 function (request, response) {
  	response.redirect('/account')
 })
 
-app.get('/auth/amazon', passport.authenticate('amazon'))
+router.get('/auth/amazon', passport.authenticate('amazon'))
 
-app.get('/auth/amazon/callback', passport.authenticate('amazon', { failureRedirect: '/' }),
+router.get('/auth/amazon/callback', passport.authenticate('amazon', { failureRedirect: '/' }),
 function (request, response) {
  	response.redirect('/account')
 })
 
-app.get('/auth/yahoo', passport.authenticate('yahoo'))
+router.get('/auth/yahoo', passport.authenticate('yahoo'))
 
-app.get('/auth/yahoo/callback', passport.authenticate('yahoo', { failureRedirect: '/' }),
+router.get('/auth/yahoo/callback', passport.authenticate('yahoo', { failureRedirect: '/' }),
 function (request, response) {
  	response.redirect('/account')
 })
 
-app.get('/auth/vimeo', passport.authenticate('vimeo'))
+router.get('/auth/vimeo', passport.authenticate('vimeo'))
 
-app.get('/auth/vimeo/callback', passport.authenticate('vimeo', { failureRedirect: '/' }),
+router.get('/auth/vimeo/callback', passport.authenticate('vimeo', { failureRedirect: '/' }),
 function (request, response) {
  	response.redirect('/account')
 })
 
-app.get('/auth/instagram', passport.authenticate('instagram'))
+router.get('/auth/instagram', passport.authenticate('instagram'))
 
-app.get('/auth/instagram/callback', passport.authenticate('instagram', { failureRedirect: '/' }),
+router.get('/auth/instagram/callback', passport.authenticate('instagram', { failureRedirect: '/' }),
 function (request, response) {
  	response.redirect('/account')
 })
@@ -78,5 +79,7 @@ function (request, response) {
 /**
  * USERS
  */
-app.get('/account', user.isAuthenticated, user.account)
-app.get('/logout', user.logout)
+router.get('/account', user.isAuthenticated, user.account)
+router.get('/logout', user.logout)
+
+module.exports = router
